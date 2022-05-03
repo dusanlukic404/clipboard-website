302e37f6d3bbcad16397a3f1dba9e4272d2df0a6
Dusan Lukic <dusanlukic404@gmail.com>
2022-05-03 10:47:49 +0200

    added changes for intersection observer

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const features = document.querySelectorAll(".feature");
+const h2 = document.querySelectorAll("h2");
 
 const revealFeatures = function (entries, observer) {
   const [entry] = entries;
@@ -11,9 +12,27 @@ const revealFeatures = function (entries, observer) {
 
 const featureObserver = new IntersectionObserver(revealFeatures, {
   root: null,
-  treshold: 1,
+  threshold: 0.5,
 });
+
 features.forEach((feature) => {
-  featureObserver.observe(feature);
   feature.classList.add("feature--hidden");
+  featureObserver.observe(feature);
+});
+
+const revealHeading2 = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("h2--hidden");
+  observer.unobserve(entry.target);
+};
+
+const heading2Observer = new IntersectionObserver(revealHeading2, {
+  root: null,
+  threshold: 1,
+});
+
+h2.forEach((heading) => {
+  heading2Observer.observe(heading);
+  heading.classList.add("h2--hidden");
 });
